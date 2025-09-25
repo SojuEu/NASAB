53ba7147208597cea221dc7a408be4fe96632919
Paulin <paulinperera007@gmail.com>
2025-09-25 19:54:05 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- js/script.js
+++ js/script.js
@@ -62,16 +62,13 @@ function validarSenha() {
 
 senha.addEventListener("input", validarSenha);
 confirmaSenha.addEventListener("input", validarSenha);
-
-senha.addEventListener("input", validarSenha);
-confirmaSenha.addEventListener("input", validarSenha);
-document.getElementById('cpf').addEventListener('input', function (e) {
+document.getElementById('cpf').addEventListener('input', function(e) {
   var value = e.target.value;
   var cpfPattern = value.replace(/\D/g, '') // Remove qualquer coisa que não seja número
-    .replace(/(\d{3})(\d)/, '$1.$2') // Adiciona ponto após o terceiro dígito
-    .replace(/(\d{3})(\d)/, '$1.$2') // Adiciona ponto após o sexto dígito
-    .replace(/(\d{3})(\d)/, '$1-$2') // Adiciona traço após o nono dígito
-    .replace(/(-\d{2})\d+?$/, '$1'); // Impede entrada de mais de 11 dígitos
+						.replace(/(\d{3})(\d)/, '$1.$2') // Adiciona ponto após o terceiro dígito
+						.replace(/(\d{3})(\d)/, '$1.$2') // Adiciona ponto após o sexto dígito
+						.replace(/(\d{3})(\d)/, '$1-$2') // Adiciona traço após o nono dígito
+						.replace(/(-\d{2})\d+?$/, '$1'); // Impede entrada de mais de 11 dígitos
   e.target.value = cpfPattern;
 });
 function validaCPF(cpf) {
@@ -82,13 +79,13 @@ function validaCPF(cpf) {
   let resto;
   if (/^(\d)\1{10}$/.test(cpf)) return false; // Verifica sequências iguais
 
-  for (let i = 1; i <= 9; i++) soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+  for (let i = 1; i <= 9; i++) soma += parseInt(cpf.substring(i-1, i)) * (11 - i);
   resto = (soma * 10) % 11;
   if ((resto === 10) || (resto === 11)) resto = 0;
   if (resto !== parseInt(cpf.substring(9, 10))) return false;
 
   soma = 0;
-  for (let i = 1; i <= 10; i++) soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+  for (let i = 1; i <= 10; i++) soma += parseInt(cpf.substring(i-1, i)) * (12 - i);
   resto = (soma * 10) % 11;
   if ((resto === 10) || (resto === 11)) resto = 0;
   if (resto !== parseInt(cpf.substring(10, 11))) return false;
@@ -96,15 +93,14 @@ function validaCPF(cpf) {
   return true;
 }
 
-document.getElementById('cpfForm').addEventListener('submit', function (e) {
+document.getElementById('cpfForm').addEventListener('submit', function(e) {
   var cpf = document.getElementById('cpf').value;
   if (!validaCPF(cpf)) {
-    e.preventDefault(); // Impede o envio do formulário
-    alert('CPF inválido. Verifique o número digitado.');
-    document.getElementById('cpf').focus(); // Foca no campo de CPF após o erro
+	e.preventDefault(); // Impede o envio do formulário
+	alert('CPF inválido. Verifique o número digitado.');
+	document.getElementById('cpf').focus(); // Foca no campo de CPF após o erro
   }
 });
-
 // const scrollSpeed = 100;
 //   let isScrolling = false;
 //   let targetScroll = 0;
